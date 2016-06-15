555e92adda64eb2b21c929e135344fd04dfd5daf
Jessica <jrame002@fiu.edu>
2016-06-15 00:49:26 -0400

    Adding Week3

---
+++ Week3/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Text;
+
+namespace Week3.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Table()
+        {
+            ViewBag.Message = "Lets create a multiplication table!";
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CreateTable(string num)
+        {
+            int n = Convert.ToInt32(num);
+
+            StringBuilder multiply = new StringBuilder();
+
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = 1; j <= n; j++)
+                {
+                    multiply.Append(i * j);
+                    multiply.Append(" ");                   
+                }
+                multiply.Append("</br>");
+            }
+            return Content(multiply.ToString());
+        }
+    }
+}
